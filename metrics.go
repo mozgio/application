b7c3db9e7f9bf441dd7dbb740dcf6aac3333f318
semen.kamenetskiy <semen.kamenetskiy@aliexpress.ru>
2023-04-09 16:50:15 +0300

    fixing metrics

--- metrics.go
+++ metrics.go
@@ -1,10 +1,10 @@
 package Application
 
 import (
-	"github.com/mozgio/application/Metrics"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-func (a *app[TConfig, TDatabase]) WithMetrics(metrics ...Metrics.Metric) App[TConfig, TDatabase] {
+func (a *app[TConfig, TDatabase]) WithMetrics(metrics ...prometheus.Collector) App[TConfig, TDatabase] {
 	a.withMetrics = true
 	a.metrics = append(a.metrics, metrics...)
 	return a
